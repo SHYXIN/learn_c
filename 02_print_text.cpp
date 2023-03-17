33475656237322223819a93ce6636696d062cc99
SHYXIN <55689295+SHYXIN@users.noreply.github.com>
2023-03-17 16:10:47 +0800

    第一次提交

---
+++ 02_print_text.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello, World\n";  // \n 新行
+    cout << "I am learning c++\n";
+    cout << "我是王鑫" << endl ;
+    return 0;
+
+}
---
+++ 03_comments.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "学习注释" << endl; // This is a comment
+    cout << "我是王鑫\n";
+    return 0;
+}
